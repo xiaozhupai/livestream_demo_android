15d78d9efa4cb7544b0e07e89972b3187aa98360
lzh <414676265@qq.com>
2017-02-28 16:59:14 +0800

    添加工具类

---
+++ app/src/main/java/cn/ucai/live/utils/MFGT.java
@@ -0,0 +1,44 @@
+package cn.ucai.live.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
+
+import cn.ucai.live.I;
+import cn.ucai.live.R;
+import cn.ucai.live.ui.activity.LoginActivity;
+import cn.ucai.live.ui.activity.MainActivity;
+
+
+/**
+ * Created by Administrator on 2017/1/10.
+ */
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+
+    }
+
+    public static void startActivity(Activity context,Class<?> clz){
+        context.startActivity(new Intent(context,clz));
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+
+    }
+    public static void startActivity(Activity context,Intent intent){
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoLogin(Activity activity){
+        startActivity(activity, LoginActivity.class);
+    }
+
+    public static void gotoMain(Activity activity){
+        startActivity(activity, new Intent(activity,MainActivity.class)
+        .putExtra(I.BACK_MAIN_FROM_CHAT,true));
+    }
+}
---
+++ app/src/main/java/cn/ucai/live/utils/OnCompleteListener.java
@@ -0,0 +1,9 @@
+package cn.ucai.live.utils;
+
+
+/**
+ * Created by LCH on 2017/1/11.
+ */
+
+public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T> {
+}
