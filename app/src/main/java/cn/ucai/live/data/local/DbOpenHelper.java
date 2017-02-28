09101c5900d47a49f237cba8be2bfb75c0b66a14
lzh <414676265@qq.com>
2017-02-28 17:16:08 +0800

    对比超级微信，添加superwechat中的本地数据保存工具类三件套

---
+++ app/src/main/java/cn/ucai/live/data/local/DbOpenHelper.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.ucai.live.data.local;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DbOpenHelper extends SQLiteOpenHelper{
+
+	private static final int DATABASE_VERSION = 1;
+	private static DbOpenHelper instance;
+
+	private static final String USERNAME_TABLE_CREATE = "CREATE TABLE "
+			+ UserDao.TABLE_NAME + " ("
+			+ UserDao.COLUMN_NAME_NICK + " TEXT, "
+			+ UserDao.COLUMN_NAME_AVATAR + " TEXT, "
+			+ UserDao.COLUMN_NAME_ID + " TEXT PRIMARY KEY);";
+	
+	private static final String CREATE_PREF_TABLE = "CREATE TABLE "
+            + UserDao.PREF_TABLE_NAME + " ("
+            + UserDao.COLUMN_NAME_DISABLED_GROUPS + " TEXT, "
+            + UserDao.COLUMN_NAME_DISABLED_IDS + " TEXT);";
+
+	private static final String USER_TABLE_CREATE = "CREATE TABLE "
+			+ UserDao.USER_TABLE_NAME + " ("
+			+ UserDao.USER_COLUMN_NAME + " TEXT PRIMARY KEY,  "
+			+ UserDao.USER_COLUMN_NAME_NICK+ " TEXT, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR_ID+ " INTEGER, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR_NAME+ " TEXT, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR_SUFFIX+ " TEXT, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR_PATH+ " TEXT, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR_TYPE+ " INTEGER, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR_UPDATE_TIME+ " TEXT); ";
+
+
+	
+	private DbOpenHelper(Context context) {
+		super(context, getUserDatabaseName(), null, DATABASE_VERSION);
+	}
+	
+	public static DbOpenHelper getInstance(Context context) {
+		if (instance == null) {
+			instance = new DbOpenHelper(context.getApplicationContext());
+		}
+		return instance;
+	}
+	
+	private static String getUserDatabaseName() {
+        return  "";//SuperWeChatHelper.getInstance().getCurrentUsernName() + "_demo.db";
+    }
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(USERNAME_TABLE_CREATE);
+		db.execSQL(CREATE_PREF_TABLE);
+		db.execSQL(USER_TABLE_CREATE);
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
+	
+	public void closeDB() {
+	    if (instance != null) {
+	        try {
+	            SQLiteDatabase db = instance.getWritableDatabase();
+	            db.close();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        instance = null;
+	    }
+	}
+	
+}
---
+++ app/src/main/java/cn/ucai/live/data/local/UserDao.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.ucai.live.data.local;
+
+import android.content.Context;
+
+import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class UserDao {
+	public static final String TABLE_NAME = "uers";
+	public static final String COLUMN_NAME_ID = "username";
+	public static final String COLUMN_NAME_NICK = "nick";
+	public static final String COLUMN_NAME_AVATAR = "avatar";
+	
+	public static final String PREF_TABLE_NAME = "pref";
+	public static final String COLUMN_NAME_DISABLED_GROUPS = "disabled_groups";
+	public static final String COLUMN_NAME_DISABLED_IDS = "disabled_ids";
+
+	public static final String USER_TABLE_NAME = "t_superwechat_user";
+	public static final String USER_COLUMN_NAME= "m_user_name";
+	public static final String USER_COLUMN_NAME_NICK = "m_user_nick";
+	public static final String USER_COLUMN_NAME_AVATAR_ID = "m_avatar_id";
+	public static final String USER_COLUMN_NAME_AVATAR_NAME = "m_avatar_user_name";			//用户账号或者群组账号
+	public static final String USER_COLUMN_NAME_AVATAR_SUFFIX ="m_avatar_suffix";              //头像后缀名
+	public static final String USER_COLUMN_NAME_AVATAR_PATH ="m_avatar_path";				//保存路径
+	public static final String USER_COLUMN_NAME_AVATAR_TYPE ="m_avatar_type";				//头像类型：\n0:用户头像\n1:群组头像
+	public static final String USER_COLUMN_NAME_AVATAR_UPDATE_TIME ="m_avatar_last_update_time";	//最后更新时间
+	
+	public UserDao(Context context) {
+	}
+
+	/**
+	 * save contact list
+	 * 
+	 * @param contactList
+	 */
+	public void saveContactList(List<EaseUser> contactList) {
+	    LiveDBManager.getInstance().saveContactList(contactList);
+	}
+
+	/**
+	 * get contact list
+	 * 
+	 * @return
+	 */
+	public Map<String, EaseUser> getContactList() {
+		
+	    return LiveDBManager.getInstance().getContactList();
+	}
+	
+	/**
+	 * delete a contact
+	 * @param username
+	 */
+	public void deleteContact(String username){
+	    LiveDBManager.getInstance().deleteContact(username);
+	}
+	
+	/**
+	 * save a contact
+	 * @param user
+	 */
+	public void saveContact(EaseUser user){
+	    LiveDBManager.getInstance().saveContact(user);
+	}
+	
+	public void setDisabledGroups(List<String> groups){
+	    LiveDBManager.getInstance().setDisabledGroups(groups);
+    }
+    
+    public List<String>  getDisabledGroups(){       
+        return LiveDBManager.getInstance().getDisabledGroups();
+    }
+    
+    public void setDisabledIds(List<String> ids){
+        LiveDBManager.getInstance().setDisabledIds(ids);
+    }
+    
+    public List<String> getDisabledIds(){
+        return LiveDBManager.getInstance().getDisabledIds();
+    }
+    
+	/**
+	 * save contact list
+	 *
+	 * @param contactList
+	 */
+	public void saveAppContactList(List<User> contactList) {
+		LiveDBManager.getInstance().saveAppContactList(contactList);
+	}
+
+	/**
+	 * get contact list
+	 *
+	 * @return
+	 */
+	public Map<String, User> getAppContactList() {
+
+		return LiveDBManager.getInstance().getAppContactList();
+	}
+
+	/**
+	 * delete a contact
+	 * @param username
+	 */
+	public void deleteAppContact(String username){
+		LiveDBManager.getInstance().deleteAppContact(username);
+	}
+
+	/**
+	 * save a contact
+	 * @param user
+	 */
+	public void saveAppContact(User user){
+		LiveDBManager.getInstance().saveAppContact(user);
+	}
+}
